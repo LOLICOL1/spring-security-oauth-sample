c33a5bfa98d1b9148e6f7b711fa91dcedeaac17f
LOLICOL1 <534619360@qq.com>
2020-08-20 11:01:50 +0800

    Add test properties

--- 5.x/security-authorization-server/src/test/java/samples/security/server/AuthorizationServerTest.java
+++ 5.x/security-authorization-server/src/test/java/samples/security/server/AuthorizationServerTest.java
@@ -1,20 +1,13 @@
 package samples.security.server;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,17 +19,6 @@ class AuthorizationServerTest {
 
     @Autowired
     MockMvc mvc;
-
-    @MockBean
-    @Qualifier("jdbcClientDetailsService")
-    ClientDetailsService clientDetailsService;
-
-    @BeforeEach
-    void setUp(@Autowired PasswordEncoder passwordEncoder) {
-        BaseClientDetails clientDetails = new BaseClientDetails("oauth-client", null, "test", "password", null, null);
-        clientDetails.setClientSecret(passwordEncoder.encode("oauth-client"));
-        when(clientDetailsService.loadClientByClientId("oauth-client")).thenReturn(clientDetails);
-    }
 
     @Test
     void requestTokenWhenUsingPasswordGrantTypeThenOk() throws Exception {
